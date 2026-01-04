03037b4b922f9c121944a2298db0f81f9bcb6897
Michal Datberg <michal.datberg@axelspringer.com>
2026-01-04 19:41:20 +0100

    REPL

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,20 @@
 use std::io::{self, Write};
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    let mut command = String::new();
-    io::stdin().read_line(&mut command).unwrap();
-    println!("{}: command not found", command.trim())
+        let mut command = String::new();
+        match io::stdin().read_line(&mut command) {
+            Ok(_) => {
+                let command = command.trim();
+                if command == "exit" {
+                    break;
+                }
+                println!("{}: command not found", command);
+            }
+            Err(_) => break,
+        }
+    }
 }
